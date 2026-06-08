669f54ccc2546c7c9850dcc103105f5ab981a0d7
Saraemilyy <mihcastro85@gmail.com>
2026-06-07 21:58:14 -0300

    Feature: Desafio 1040 - Media 3 Pendente: Validação código

--- Leetcode/1040-Medium.cs
+++ Leetcode/1040-Medium.cs
@@ -1,9 +1,58 @@
-/*Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
 
-No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno.
+// Exemplo de Entrada	Exemplo de Saída
+// 2.0 4.0 7.5 8.0
+// 6.4
 
-Entrada
-A entrada contém quatro números de ponto flutuante correspendentes as notas dos alunos.
+// Media: 5.4
+// Aluno em exame.
+// Nota do exame: 6.4
+// Aluno aprovado.
+// Media final: 5.9
+class Program
+{
+ public static void Main(string[] args)
+ {
 
-Saída
-Todas as respostas devem ser apresentadas com uma casa decimal. As mensagens devem ser impressas conforme a descrição do problema. Não esqueça de imprimir o enter após o final de cada linha, caso contrário obterá "Presentation Error".*/
+  string[] entradaNotas = Console.ReadLine().Split(' ');
+  double nota1 = double.Parse(entradaNotas[0]);
+  double nota2 = double.Parse(entradaNotas[1]);
+  double nota3 = double.Parse(entradaNotas[2]);
+  double nota4 = double.Parse(entradaNotas[3]);
+  double nota5 = double.Parse(entradaNotas[4]);
+  CalcularMedia( nota1, nota2, nota3, nota4, nota5);
+
+  string CalcularMedia (double nota1, double nota2, double nota3, double nota4, double nota5)
+  {
+   int peso1 = 2;
+   int peso2 = 3;
+   int peso3 = 4;
+   int peso4 = 1;
+
+   int totalPeso = peso1 + peso2 + peso3 + peso4;
+   double mediaPonderada = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4) / totalPeso;
+
+   if (mediaPonderada >= 7.00)
+   {
+    string resultado = $"Média: {mediaPonderada:0}\nAluno Aprovado";
+    return resultado;
+   }
+   else if (mediaPonderada >= 0.00 && mediaPonderada <= 5.00)
+   {
+    string resultado = $"Média: {mediaPonderada:0}\nAluno Reprovado";
+    return resultado;
+   }
+   else
+   {
+    double resultadoFinal = (mediaPonderada + nota5) /2;
+    if (resultadoFinal >= 5.00)
+    {
+     string resultado = $"Média: {mediaPonderada:0}\nAluno em exame.\nNota do exame: {nota5}\nAluno Aprovado\nMédia FInal: {resultadoFinal}";
+     return resultado;
+    }else{
+     string resultado = $"Média: {mediaPonderada:0}\nAluno em exame.\nNota do exame: {nota5}\nAluno Reprovado\nMédia Fnal: {resultadoFinal}";
+     return resultado;
+    }
+   }
+  }
+ }
+}
