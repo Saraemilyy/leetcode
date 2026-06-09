c304b9f961f40225f555921c09f2d1cb78ea09d7
Saraemilyy <mihcastro85@gmail.com>
2026-06-08 22:47:26 -0300

    fix: Desafio 1040 Corrigido, funcionando.

--- Leetcode/1038-Food.cs
+++ Leetcode/1038-Food.cs
@@ -1,30 +1,25 @@
 
-using System;
-using System.Globalization;
-using System.Collections.Generic;
+//using System;
+//using System.Globalization;
+//using System.Collections.Generic;
+//namespace LeetCode;
+//public class Solution1038
+//{
+// public static string[] entrada = Console.ReadLine().Split(' ');
 
-public class Solution1038
-{
- public static void Main(string[] args)
- {
-  string[] entrada = Console.ReadLine().Split(' ');
+//  int codigo = int.Parse(entrada[0]);
+//  int quantidade = int.Parse(entrada[1]);
 
-  int codigo = int.Parse(entrada[0]);
-  int quantidade = int.Parse(entrada[1]);
+//  Dictionary<int, double> cardapio = new()
+//        {
+//            {1, 4.00},
+//            {2, 4.50},
+//            {3, 5.00},
+//            {4, 2.00},
+//            {5, 1.50}
+//        };
 
-  Dictionary<int, double> cardapio = new()
-        {
-            {1, 4.00},
-            {2, 4.50},
-            {3, 5.00},
-            {4, 2.00},
-            {5, 1.50}
-        };
+//  double total = cardapio[codigo] * quantidade;
 
-  double total = cardapio[codigo] * quantidade;
-
-  Console.WriteLine(
-      $"Total: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}"
-  );
- }
-}
+//  Console.WriteLine($"Total: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+// }
--- Leetcode/1040-Medium.cs
+++ Leetcode/1040-Medium.cs
@@ -8,51 +8,70 @@
 // Nota do exame: 6.4
 // Aluno aprovado.
 // Media final: 5.9
-class Program
+namespace LeetCode;
+
+
+class Solution1040
 {
- public static void Main(string[] args)
- {
 
-  string[] entradaNotas = Console.ReadLine().Split(' ');
-  double nota1 = double.Parse(entradaNotas[0]);
-  double nota2 = double.Parse(entradaNotas[1]);
-  double nota3 = double.Parse(entradaNotas[2]);
-  double nota4 = double.Parse(entradaNotas[3]);
-  double nota5 = double.Parse(entradaNotas[4]);
-  CalcularMedia( nota1, nota2, nota3, nota4, nota5);
 
-  string CalcularMedia (double nota1, double nota2, double nota3, double nota4, double nota5)
-  {
-   int peso1 = 2;
-   int peso2 = 3;
-   int peso3 = 4;
-   int peso4 = 1;
+    //    double nota1 = double.Parse(entradaNotas[0]);
+    //    double nota2 = double.Parse(entradaNotas[1]);
+    //    double nota3 = double.Parse(entradaNotas[2]);
+    //    double nota4 = double.Parse(entradaNotas[3]);
 
-   int totalPeso = peso1 + peso2 + peso3 + peso4;
-   double mediaPonderada = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4) / totalPeso;
+    //    double? nota5 = null;
 
-   if (mediaPonderada >= 7.00)
-   {
-    string resultado = $"Média: {mediaPonderada:0}\nAluno Aprovado";
-    return resultado;
-   }
-   else if (mediaPonderada >= 0.00 && mediaPonderada <= 5.00)
-   {
-    string resultado = $"Média: {mediaPonderada:0}\nAluno Reprovado";
-    return resultado;
-   }
-   else
-   {
-    double resultadoFinal = (mediaPonderada + nota5) /2;
-    if (resultadoFinal >= 5.00)
+    //if (entradaNotas.Length > 4)
+    //{
+    //    nota5 = double.Parse(entradaNotas[4]);
+    //}
+
+
+    //Console.WriteLine(
+    //    CalcularMedia(
+    //        nota1,
+    //        nota2,
+    //        nota3,
+    //        nota4,
+    //        nota5));
+    //    }
+    public static string CalcularMedia(double nota1, double nota2, double nota3, double nota4, double? nota5 = null)
     {
-     string resultado = $"Média: {mediaPonderada:0}\nAluno em exame.\nNota do exame: {nota5}\nAluno Aprovado\nMédia FInal: {resultadoFinal}";
-     return resultado;
-    }else{
-     string resultado = $"Média: {mediaPonderada:0}\nAluno em exame.\nNota do exame: {nota5}\nAluno Reprovado\nMédia Fnal: {resultadoFinal}";
-     return resultado;
+        int peso1 = 2;
+        int peso2 = 3;
+        int peso3 = 4;
+        int peso4 = 1;
+
+        int totalPeso = peso1 + peso2 + peso3 + peso4;
+        double mediaPonderada = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4)) / totalPeso;
+        mediaPonderada = Math.Truncate(mediaPonderada * 10) / 10.0;
+        if (mediaPonderada >= 7.00)
+        {
+            string resultado = $"Media: {mediaPonderada:f1}\nAluno aprovado.";
+            return resultado;
+        }
+        else if (mediaPonderada >= 0.00 && mediaPonderada < 5.00)
+        {
+            string resultado = $"Media: {mediaPonderada:f1}\nAluno reprovado.";
+            return resultado;
+        }
+        else
+        {
+            double notaExame = double.Parse(Console.ReadLine());
+
+            double mediaFinal = (mediaPonderada + notaExame) / 2;
+
+            if (mediaFinal >= 5.00)
+            {
+                string resultado = $"Media: {mediaPonderada:f1}\nAluno em exame.\nNota do exame: {notaExame:f1}\nAluno aprovado.\nMedia final: {mediaFinal:f1}";
+                return resultado;
+            }
+            else
+            {
+                string resultado = $"Media: {mediaPonderada:f1}\nAluno em exame.\nNota do exame: {nota5:f1}\nAluno reprovado.\nMedia final: {mediaFinal:f1}";
+                return resultado;
+            }
+        }
     }
-   }
-  }
- }
 }
