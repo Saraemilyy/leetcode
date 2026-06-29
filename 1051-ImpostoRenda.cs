4e64fd272ab97ec6de17c9cb1f615210893b8df6
Saraemilyy <mihcastro85@gmail.com>
2026-06-29 18:20:35 -0300

    Feature: New Solution 1066

---
+++ 1051-ImpostoRenda.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode;
+
+public class Solution1051
+{
+
+
+
+        double salarioEntrada = double.Parse(Console.ReadLine());
+
+        if (salarioEntrada >= 0 && salarioEntrada <= 2000)
+        {
+            Console.WriteLine("Isento");
+        }
+        else if (salarioEntrada >= 2000.01 && salarioEntrada <= 3000)
+        {
+            double salarioImpostoFinal = ((salarioEntrada - 2000) * 8) / 100;
+            Console.WriteLine($"R${salarioImpostoFinal}");
+
+
+        }
+        else if (salarioEntrada >= 3000.01 && salarioEntrada <= 4500)
+        {
+            if (salarioEntrada - 2000 > 1000)
+            {
+                double salarioImpostoCom8 = ((salarioEntrada - 2000 - (1000 - (salarioEntrada - 2000))) * 8) / 100;
+
+                double salarioImpostoCom18 = (salarioEntrada - 3000);
+
+                double resultadoFinal = salarioImpostoCom8 + salarioImpostoCom18;
+
+                Console.WriteLine($"R${resultadoFinal}");
+                Console.WriteLine($"R${salarioImpostoCom18}");
+                Console.WriteLine($"R${salarioImpostoCom8}");
+                Console.WriteLine($"R${salarioEntrada}");
+
+            }
+
+            else
+            {
+                double salarioImpostoFinal = ((salarioEntrada - 2000) * 8) / 100;
+                Console.WriteLine($"R${salarioImpostoFinal}");
+            }
+
+        }
+        else
+        {
+            //acima de 4500
+            //Console.WriteLine($"R${valorImposto}");
+        }
+
+
+    }
---
+++ 1066-ParesImparesPositivosNegativos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode;
+
+public class _1066_ParesImparesPositivosNegativos
+{
+    int[] linhas = new int[5];
+    int valoresPares = 0;
+    int valoresImpares = 0;
+    int valoresPositivos = 0;
+    int valoresNegativos = 0;
+        
+        // Laço para ler e armazenar as 5 entradas
+        for (int i = 0; i< 5; i++) {
+        linhas[i] = int.Parse(Console.ReadLine());
+        if (linhas[i] % 2 == 0)
+        {
+          valoresPares++;
+        } 
+        else{valoresImpares++;}
+      
+        if (linhas[i] > 0)
+{ valoresPositivos++; }
+if (linhas[i] < 0)
+{ valoresNegativos++; }
+        
+        
+        }
+       
+
+    Console.WriteLine($"{valoresPares} valor(es) par(es)");
+Console.WriteLine($"{valoresImpares} valor(es) impar(es)");
+Console.WriteLine($"{valoresPositivos} valor(es) positivo(s)");
+Console.WriteLine($"{valoresNegativos} valor(es) negativo(s)");
+
+}
