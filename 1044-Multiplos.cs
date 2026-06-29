6a416f2bfbef752d515964c66a15af80ba069096
Saraemilyy <mihcastro85@gmail.com>
2026-06-28 21:32:54 -0300

    Feature: New solution 1044

---
+++ 1044-Multiplos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode;
+
+public class Solution1044
+{
+    public static void VerificarMultiplos() { 
+
+    string entrada = Console.ReadLine();
+
+    int[] numeros = Array.ConvertAll(entrada.Split(' '), int.Parse);
+    int numero1 = numeros[0];
+    int numero2 = numeros[1];
+    int calculo = numero2 % numero1; 
+
+   if (numero1 % numero2 == 0 || numero2 % numero1 == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+    else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+    }
+}
